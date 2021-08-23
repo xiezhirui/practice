dd454442fc30a86cc7b3cd32ca9f1c486999937b
xiezhirui <xiezr@cdhuari.com>
2021-08-23 22:13:41 +0800

    2021.8.23 谢直锐创建

---
+++ src/main/java/com/myhrtx/practice/Test.java
@@ -0,0 +1,15 @@
+package com.myhrtx.practice;
+/**
+ * 2021.8.11
+ * 《Java架构师指南》
+ * @author xzr
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("你好 Java");
+	}
+
+}
